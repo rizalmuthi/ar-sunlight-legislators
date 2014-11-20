6808db9d429611daacb7eb5b61d59d4a51b1070f
Rizal Muthi <rizalmuthi@gmail.com>
2014-11-21 00:26:14 +0800

    Complete the legislator importer

--- lib/sunlight_legislators_importer.rb
+++ lib/sunlight_legislators_importer.rb
@@ -1,14 +1,15 @@
 require 'csv'
+require_relative '../app/models/legislator'
 
 class SunlightLegislatorsImporter
   def self.import(filename)
     csv = CSV.new(File.open(filename), :headers => true)
     csv.each do |row|
+      hash = {}
       row.each do |field, value|
-        # TODO: begin
-        raise NotImplementedError, "TODO: figure out what to do with this row and do it!"
-        # TODO: end
+        hash[field] = value
       end
+      Legislator.create(hash)
     end
   end
 end
